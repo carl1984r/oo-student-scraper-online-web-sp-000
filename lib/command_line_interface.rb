7ee584410b371e4bcced903f7492d4aabfcc3b47
Bry Cox <carl.cc@gmail.com>
2019-04-09 14:11:15 +0000

    Automatically backed up by Learn

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -29,7 +29,7 @@ class CommandLineInterface
       puts "#{student.name.upcase}".colorize(:blue)
       puts "  location:".colorize(:light_blue) + " #{student.location}"
       puts "  profile quote:".colorize(:light_blue) + " #{student.profile_quote}"
-      puts "  bio:".colorize(:light_blue) + " #{student.bio}"
+      puts "  bio:".colorize(:green) + " #{student.bio}".colorize(:green)
       puts "  twitter:".colorize(:light_blue) + " #{student.twitter}"
       puts "  linkedin:".colorize(:yellow) + " #{student.linkedin}".colorize(:yellow)
       puts "  github:".colorize(:light_blue) + " #{student.github}"
